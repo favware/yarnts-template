1111cbd6ebea3a8feb8ab448714cc3997aa171f2
Jeroen Claassens <support@favware.tech>
2019-08-17 14:12:17 +0200

    test: better test coverage and include snapshot testing

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,5 +1,21 @@
-describe('Sample', () => {
-  test('Sample', () => {
-    expect(true).toBe(true);
+import defaultSample, {sampleFunc} from '../src';
+
+describe('Named Import Sample', () => {
+  test('should work with named import', () => {
+    expect(sampleFunc('sample-named')).toBe('sample-named');
+  });
+
+  test('should match snapshot', () => {
+    expect(sampleFunc).toMatchSnapshot();
+  });
+});
+
+describe('Default Import Sample', () => {
+  test('should work with default import', () => {
+    expect(defaultSample('sample-default')).toBe('sample-default');
+  });
+
+  test('should match snapshot', () => {
+    expect(defaultSample).toMatchSnapshot();
   });
 });
